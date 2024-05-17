d52d6995ac8ba19a2ec082be4dbed0b54ac8ae10
DELL <DELL@DESKTOP-04S95NF>
2024-05-17 08:42:28 +0530

    Add project files.

---
+++ MVCWithEFCF8/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace MVCWithEFCF8.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MVCWithEFCF8.Models.SchoolDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "MVCWithEFCF8.Models.SchoolDbContext";
+        }
+
+        protected override void Seed(MVCWithEFCF8.Models.SchoolDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ MVCWithEFCF8/Models/SchoolDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using MVCWithEFCF8.Migrations;
+
+namespace MVCWithEFCF8.Models
+{
+    public class SchoolDbContext : DbContext
+
+    {
+        public SchoolDbContext():base("ConStr")
+            {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<SchoolDbContext,Configuration>());
+
+            }
+        public DbSet<Student> Students { get; set; }
+
+    }
+}
